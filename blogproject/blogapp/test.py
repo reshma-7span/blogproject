e2d36cb1bd2928a9250dd2d05596eb721fbe6e40
reshma <reshma@7span.com>
2023-07-21 19:36:14 +0530

    add file

---
+++ blogproject/blogapp/test.py
@@ -0,0 +1,10 @@
+username = 'testuser'
+password = 'testuser'
+title = 'testtitle'
+body = 'testbody'
+comments = 'testcomment'
+test_blog_list_view ="blog_list"
+test_blog_detail_view = "blog_detail"
+test_create_blog_view= "username='testuser', password='testpassword'"('create blog')
+test_update_blog_view="username='testuser', password='testpassword'", " 'title': 'Updated Blog', 'body': 'Updated body'"('update blog')
+test_delete_blog_view="username='testuser', password='testpassword'"('delete blog with id')
